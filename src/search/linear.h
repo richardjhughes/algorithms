89f46f6211b57bbbfe640b2e804ce9902377f151
Richard Hughes <richard.j.hughes@hotmail.com>
2024-01-07 17:47:10 -0500

    Implement linear search algorithms.

--- src/search/linear.h
+++ src/search/linear.h
@@ -1,5 +1,71 @@
 #pragma once
 
+#include <vector>
+
 namespace algorithms::search::linear {
-    int hi() { return 123; }
+
+// all algorithms here are based on this source:
+// https://en.wikipedia.org/wiki/Linear_search
+
+// performs a linear search on an unordered `set` to find `target`
+// complexity: O(n)
+// @param set the set to search
+// @param target the element to search for
+// @returns true if found, else false
+template <typename T>
+[[nodiscard]] auto linear_basic(const std::vector<T> &set, const T &target)
+    -> bool {
+  for (const auto &element : set) {
+    if (element == target) {
+      return true;
+    }
+  }
+
+  return false;
 }
+
+// Performs a linear search on an unordered `set` to find `target`.
+// It inserts a sentinel value, `target`, to the end of
+// the list and guarantees the search ends of this sentinel value is hit.
+// This is to remove the conditional check of `index < std::size(set)`.
+// complexity: O(n)
+// @param set the set to search
+// @param target the element to search for
+// @returns true if found, else false
+template <typename T>
+[[nodiscard]] auto linear_sentinel(std::vector<T> &set, const T &target)
+    -> bool {
+  set.push_back(target);
+
+  auto index{0u};
+  while (set[index++] != target)
+    ;
+
+  // return false if the sentinel value was hit
+  return index - 1 != std::size(set) - 1;
+}
+
+// Performs a linear search on an ordered `set` to find `target`.
+// It inserts a sentinel value, `target + 1`, to the end of
+// the list and guarantees the search ends of this sentinel value is hit.
+// Instead of checking if `set[i] == target`, this checks if `set[i] > target`.
+// This enables an early exit from the loop, as if the element is greater than
+// `target`, `target` will not be in `set`.
+// This is to remove the conditional check of `index < std::size(set)`.
+// complexity: O(n)
+// @param set the set to search
+// @param target the element to search for
+// @returns true if found, else false
+template <typename T>
+[[nodiscard]] auto linear_sentinel_ordered(std::vector<T> &set, const T &target)
+    -> bool {
+  set.push_back(target + 1);
+
+  auto index{0u};
+  while (set[index++] < target)
+    ;
+
+  const auto result = set[index - 1] == target;
+  return result;
+}
+} // namespace algorithms::search::linear
--- src/tests/search/linear.cpp
+++ src/tests/search/linear.cpp
@@ -1,9 +1,70 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include <ranges>
+
 #include "search/linear.h"
 
 using namespace algorithms::search::linear;
 
-TEST_CASE("hi", "[name]") {
-    REQUIRE(hi() > 0);
+TEST_CASE("can find item using linear search basic", "[algorithm][search][linear]") {
+    std::vector<std::uint32_t> set {};
+
+    for (auto i {0u}; i < 10000; ++i) {
+        set.push_back(i);
+    }
+
+    REQUIRE(linear_basic<std::uint32_t>(set, 0));
+    REQUIRE(linear_basic<std::uint32_t>(set, 5000));
+    REQUIRE(linear_basic<std::uint32_t>(set, 9999));
+    REQUIRE_FALSE(linear_basic<std::uint32_t>(set, 10000));
+}
+
+TEST_CASE("can find item using linear search sentinel", "[algorithm][search][linear]") {
+    std::vector<std::uint32_t> set {};
+
+    for (auto i {0u}; i < 10000; ++i) {
+        set.push_back(i);
+    }
+
+    // we need to use sections because `set` will be modified
+    SECTION("find 0") {
+        REQUIRE(linear_sentinel<std::uint32_t>(set, 0));
+    }
+
+    SECTION("find 5000") {
+        REQUIRE(linear_sentinel<std::uint32_t>(set, 5000));
+    }
+
+    SECTION("find 9999") {
+        REQUIRE(linear_sentinel<std::uint32_t>(set, 9999));
+    }
+
+    SECTION("does not find 10000") {
+        REQUIRE_FALSE(linear_sentinel<std::uint32_t>(set, 10000));
+    }
+}
+
+TEST_CASE("can find item using linear search sentinel ordered", "[algorithm][search][linear]") {
+    std::vector<std::uint32_t> set {};
+
+    for (auto i {0u}; i < 10000; ++i) {
+        set.push_back(i);
+    }
+
+    // we need to use sections because `set` will be modified
+    SECTION("find 0") {
+        REQUIRE(linear_sentinel_ordered<std::uint32_t>(set, 0));
+    }
+
+    SECTION("find 5000") {
+        REQUIRE(linear_sentinel_ordered<std::uint32_t>(set, 5000));
+    }
+
+    SECTION("find 9999") {
+        REQUIRE(linear_sentinel_ordered<std::uint32_t>(set, 9999));
+    }
+
+    SECTION("does not find 10000") {
+        REQUIRE_FALSE(linear_sentinel_ordered<std::uint32_t>(set, 10000));
+    }
 }
